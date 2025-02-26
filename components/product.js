6d708255f14d50e65657fbfa0f1d105adf1fb2fe
Andrea <ca.calzavara@donboscosandona.it>
2025-02-26 08:49:43 +0100

    27

---
+++ components/product.js
@@ -0,0 +1,25 @@
+export default function product() {
+    return(
+      <>
+      <div className="flex items-center justify-center min-h-screen bg-slate-100">
+      <div className="flex flex-col p-6 m-3 space-y-10 bg-white rounded-2xl shadow-2xl md:flex-row md:space-y-0 md:space-x-10 md:m-0 md:p-16">
+      </div>
+        <img 
+        src=""
+        alt=""
+        className="mx-auto duration-200 w-60 hover:scale-105"
+        />
+      </div>
+        
+      <div className="flex flex-col space-y-6">
+      <div className="flex flex-col mb-4 space-y-3 text-center md:text-left">
+      </div>
+      <div className="inline">
+
+      </div>
+
+      </div>
+
+      </>
+    );
+}
